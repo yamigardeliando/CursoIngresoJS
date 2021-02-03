4e546ed3a900e50ebb6e0600fa38962178a9983c
yamila <yamilakp12@hotmail.com>
2021-02-03 10:16:54 -0300

    eentrada y salida trabajado

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,7 +2,40 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
+
+
 {
-	alert("ok");
+	// ID txtIdImporte  id="txtIdResultado"//
+
+var importeingresado;
+importeingresado= txtIdImporte.value; 
+alert (importeingresado);
+
+
+
+
+
+
+
+{
+	/*
+	var nombre;
+	var apellido;
+	//prompt : es para la entrada de datos, el usuario ingresa su nombre apellido//
+	nombre=prompt ("ingrese su nombre ");
+	apellido=prompt ("ingrese su apellido ");
+	//alert: es para la salida de datos// 
+	alert ( apellido);
+
+//alert (dato);// ejemplo donde pido que me muestre una palabra "dato" pero qe no esta definida como nada, ni como variable, ni como valor//
+alert (nombre); //ejemplo de"undefined" pido que me muestre una variable a la que aun no le asigne valor//
+nombre= "maria";
+alert(nombre);
+alert("su nombre es "+ nombre);
+
+
 }
+
+/* asi es como hacemos comentarios con esta modalidad de barras y asteriscos
+*/
 
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,7 +3,11 @@ Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-function mostrarAumento()
-{
-	alert("ok");
+function mostrarAumento() {
+  // ID txtIdImporte  id="txtIdResultado"//
+
+  var importeingresado;
+  //importeingresado= txtIdImporte.value;//
+  importeingresado = document.getElementById("txtIdImporte").value;
+  txtIdResultado.value = "el importe es: " + importeingresado;
 }
